8a5be0ac67cbb5f43115a244f0982509f391d791
Yngve Bakken Nilsen <yngve.nilsen@gmail.com>
2013-04-24 09:30:41 +0200

    Docs-list

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,17 @@
 var ngRaven = angular.module("ngRaven", ['ngResource'])
 
+ngRaven.config(function($routeProvider){
+	$routeProvider
+		.when("/", {
+			templateUrl: "views/database-list.html",
+			controller: "ListCtrl"
+		})
+		.when("/database/:name", {
+			templateUrl: "views/docs-list.html",
+			controller: "DocsCtrl"
+		})
+});
+
 ngRaven.factory("Raven", function($http){
 	var self = this;
 	self.host = 'http://localhost\:8088';
@@ -19,9 +31,17 @@ ngRaven.factory("Raven", function($http){
 });
 
 ngRaven.controller('ListCtrl', function($scope, Raven){
-	Raven.databases('dummy').success(function(data){
+	Raven.databases().success(function(data){
 		$scope.items = data;
 		console.log(data);
 	});
 	
+});
+
+ngRaven.controller("DocsCtrl", function($scope, Raven, $routeParams){
+	console.log('loading docs for '+$routeParams.name);
+	Raven.databases($routeParams.name).success(function(data){
+		console.log(data);
+		$scope.docs = data;
+	})
 });
